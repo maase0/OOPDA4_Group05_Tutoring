c96fcd4acf3d2147c7275f84d3026d719dbb89b1
knots <knotsofunny@gmail.com>
2018-02-02 15:11:58 -0500

    Implementing some basic classes

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Erich Maas
+ * @author 
+ * @author 
+ * @author
+ */
+public class Student {
+
+	private String name;
+	
+	public Student(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/Tutor.java
@@ -0,0 +1,12 @@
+
+public class Tutor {
+	
+	private String name;
+	private String year;
+	
+	public Tutor(String name, String year) {
+		this.name = name;
+		this.year = year;
+	}
+
+}
