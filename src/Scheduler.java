f0ea5b0028d9d4656900c2b320c9188d35898985
knots <knotsofunny@gmail.com>
2018-02-03 12:10:46 -0500

    CommitMessage

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Scheduler {
 
@@ -7,6 +8,69 @@ public class Scheduler {
 	public Scheduler() {
 		schedule = new HashMap<Day, HashMap<Integer, Pair>>();
 		initializeSchedule();
+	}
+	
+	/**
+	 * Returns the entire schedule
+	 * @return The schedule
+	 */
+	public HashMap<Day, HashMap<Integer, Pair>> getSchedule() {
+		return schedule;
+	}
+	
+	public void getScheduleAsArray() {
+		Pair[][] scheduleArray = new Pair[5][(1745 - 1000) % 15];
+		
+		int dayCount = 0;
+		for(Day day : Day.values()) {
+			int timeCount = 0;
+			for(Integer i : schedule.get(day).keySet()) {
+				scheduleArray[dayCount][timeCount] = schedule.get(day).get(timeCount + 1000);
+				
+			}
+		}
+	}
+	
+	/**
+	 * Returns the timetable for a given day
+	 * @param day The day to get the timetable for
+	 * @return The time-pair hashmap timetable for a given day
+	 */
+	public HashMap<Integer, Pair> getTimetable(Day day) {
+		return schedule.get(day);
+	}
+	
+	/**
+	 * Checks if any tutor available for a given time, day, and duration.
+	 * @param day The day to check
+	 * @param startTime The start time to check
+	 * @param duration The duration of the appointment
+	 * @return true if there is a tutor available
+	 */
+	public boolean checkAvailability(Day day, int startTime, int duration) {
+		boolean available = false;
+		HashMap<Integer, Pair> timetable = schedule.get(day);
+		
+		if(timetable.get(startTime) != null) {
+			available = true;
+			
+			Tutor t = timetable.get(startTime).getTutor();
+			for(int i = startTime + 15; i < startTime + duration; i += 15) {
+				if(!(timetable.get(i) != null && timetable.get(i).getTutor().equals(t))) {
+					available = false;
+				}
+			}
+		}
+		
+		return available;
+	}
+	
+	public boolean findAvailability(Tutor tutor, int duration) {
+		boolean availability = false;
+		ArrayList<>
+		
+		
+		return availability;
 	}
 	
 	/**
@@ -44,11 +108,9 @@ public class Scheduler {
 	 * Initializes the schedule of all possible days
 	 */
 	private void initializeSchedule() {
-		schedule.put(Day.MONDAY, new HashMap<Integer, Pair>());
-		schedule.put(Day.TUESDAY, new HashMap<Integer, Pair>());
-		schedule.put(Day.WEDNESDAY, new HashMap<Integer, Pair>());
-		schedule.put(Day.THURSDAY, new HashMap<Integer, Pair>());
-		schedule.put(Day.FRIDAY, new HashMap<Integer, Pair>());
+		for(Day day : Day.values()) {
+			schedule.put(day, new HashMap<Integer, Pair>());
+		}
 	}
 	
 	
