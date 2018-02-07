3c49361de40a66ab9aa7a71ddef20b7e67175635
knots <knotsofunny@gmail.com>
2018-02-07 16:48:23 -0500

    Adding checking availability by tutor and day

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,28 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		Scheduler s = new Scheduler();
+		Scheduler sh = new Scheduler();
 		Tutor t = new Tutor("Guy Person", "senior");
+		Student s = new Student("Somebody");
 		
-		s.addTutor(Day.MONDAY, 1500, t);
+		sh.addTutor(Day.MONDAY, 1500, t);
+		sh.addTutor(Day.MONDAY, 1515, t);
+		sh.addTutor(Day.MONDAY, 1530, t);
+		sh.addTutor(Day.MONDAY, 1545, t);
+		sh.addTutor(Day.MONDAY, 1600, t);
+		sh.addTutor(Day.MONDAY, 1615, t);
+
+		sh.addStudent(Day.MONDAY, 1500, s);
 		
-		printSchedule(s);
+		printSchedule(sh);
+		System.out.println(sh.checkAvailabilityByTutor(t, Day.MONDAY, 30));
 	}
 	
 	public static void printSchedule(Scheduler s) {
+		int count = 0;
 		for(String row : s.convertToStringArray()) {
-			System.out.println(row);
+			System.out.println(count + "   " + row);
+			count++;
 		}
 	}
 	
--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,66 +1,89 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Scheduler {
 
 	public static final int DAYS = 5;
 	public static final int BLOCK_LENGTH = 15;
 	public static final int NUM_BLOCKS = 32; // 1000 to 1800: 8 hours * 4 fifteen min blocks per hour
-	
+
 	private Pair[][] schedule;
-	
+
 	public Scheduler() {
 		schedule = new Pair[DAYS][NUM_BLOCKS];
 		initializeSchedule();
 	}
-	
+
 	/**
 	 * Returns the entire schedule
+	 * 
 	 * @return The schedule
 	 */
 	private Pair[][] getSchedule() {
 		return schedule;
 	}
-	
-	
-	
+
+	public ArrayList<Integer> checkAvailabilityByTutor(Tutor tutor, Day day, int duration ) {
+		ArrayList<Integer> availableTimes = new ArrayList<Integer>();
+		
+			for(int time = 0; time < NUM_BLOCKS - duration % BLOCK_LENGTH; time++) {
+				boolean available = true;
+				for(int i = 0; i < duration / BLOCK_LENGTH && available; i++) {
+					if((!tutor.equals(schedule[day.value()][i].getTutor())) || 
+							schedule[day.value()][i].getStudent() != null ) {
+						available = false;
+					}
+				}
+				if(available) {
+					availableTimes.add(time);
+				}
+			}
+		
+		
+		return availableTimes;
+	}
+
 	/**
 	 * Checks if any tutor available for a given time, day, and duration.
+	 * 
 	 * @param day The day to check
 	 * @param startTime The start time to check
-	 * @param duration The duration of the appointment
+	 * @param duration  The duration of the appointment
 	 * @return true if there is a tutor available for the given time and duration
 	 */
 	public boolean checkAvailability(Day day, int startTime, int duration) {
 		boolean available = false;
-		
+
 		Tutor t = schedule[day.value()][timeToArrayIndex(startTime)].getTutor();
-		if(t != null) {
+		if (t != null) {
 			available = true;
-			for(int i = 15; i < duration; i += 15) {
-				if(! t.equals((schedule[day.value()][timeToArrayIndex(startTime)].getTutor())) && available) {
+			for (int i = 15; i < duration; i += 15) {
+				if (!t.equals((schedule[day.value()][timeToArrayIndex(startTime)].getTutor())) && available) {
 					available = false;
 				}
 			}
 		}
 		return available;
 	}
-	
-	
-	
+
 	/**
-	 * Adds a tutor to a given time and day only if there is not already a tutor
-	 * at that time and day
+	 * Adds a tutor to a given time and day only if there is not already a tutor at
+	 * that time and day
+	 * 
 	 * @param day The day to schedule the tutor
 	 * @param time The time to schedule the tutor
 	 * @param tutor The tutor to schedule
 	 */
 	public void addTutor(Day day, int time, Tutor tutor) {
-		if(schedule[day.value()][timeToArrayIndex(time)].getTutor() == null) {
+		if (schedule[day.value()][timeToArrayIndex(time)].getTutor() == null) {
 			schedule[day.value()][timeToArrayIndex(time)].setTutor(tutor);
 		}
 	}
-	
+
 	/**
 	 * Sets the student of an existing tutor-student pair on the given day and time
 	 * Currently allow overwriting a current student
+	 * 
 	 * @param day The day to add the student
 	 * @param time The time to add the student
 	 * @param student The student to be added
@@ -68,9 +91,10 @@ public class Scheduler {
 	public void addStudent(Day day, int time, Student student) {
 		schedule[day.value()][timeToArrayIndex(time)].setStudent(student);
 	}
-	
+
 	/**
 	 * Converts the time from 24 hour HHMM format to a value from 0 to 31
+	 * 
 	 * @param time The time to convert
 	 * @return An array index for the schedule array
 	 */
@@ -78,37 +102,39 @@ public class Scheduler {
 		return (((int) time / 100) - 10) * 4 + ((time % 100) / 15);
 	}
 	
+	private int arrayIndexToTime(int index) {
+		return 0;
+	}
+
 	/**
-	 * Initializes the schedule to all null
-	 * Maybe not needed
+	 * Initializes the schedule to all null Maybe not needed
 	 */
 	private void initializeSchedule() {
-		for(int i = 0; i < DAYS; i++) {
-			for(int j = 0; j < NUM_BLOCKS; j++) {
+		for (int i = 0; i < DAYS; i++) {
+			for (int j = 0; j < NUM_BLOCKS; j++) {
 				schedule[i][j] = new Pair();
 			}
 		}
 	}
-	
+
 	/**
-	 * Converts the schedule to an array of strings where each row is an element
-	 * of the array and columns are separated by a space
+	 * Converts the schedule to an array of strings where each row is an element of
+	 * the array and columns are separated by a space
 	 */
 	public String[] convertToStringArray() {
 		String[] rows = new String[Scheduler.NUM_BLOCKS];
 		Arrays.fill(rows, "");
-		
-		for(int i = 0; i < Scheduler.DAYS; i++) {
-			for(int j = 0; j < Scheduler.NUM_BLOCKS; j++) {
+
+		for (int i = 0; i < Scheduler.DAYS; i++) {
+			for (int j = 0; j < Scheduler.NUM_BLOCKS; j++) {
 				rows[j] += schedule[i][j].getTutor() == null ? "NONE" : schedule[i][j].getTutor().getName();
 				rows[j] += ": ";
 				rows[j] += schedule[i][j].getStudent() == null ? "NONE" : schedule[i][j].getStudent().getName();
 				rows[j] += " ";
 			}
 		}
-		
+
 		return rows;
 	}
-	
-	
+
 }
