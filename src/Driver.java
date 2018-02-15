3ea6cd3cf27fb165d01769653c66a08a21e89904
knots <knotsofunny@gmail.com>
2018-02-15 16:04:34 -0500

    test commit

--- src/Driver.java
+++ src/Driver.java
@@ -1,28 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		/*Scheduler sh = new Scheduler();
-		Tutor t = new Tutor("Guy Person", "senior");
-		Student s = new Student("Somebody");
+		Scheduler sh = new Scheduler();
+		static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 		
-		sh.addTutor(Day.MONDAY, 1000, t);
-		sh.addTutor(Day.MONDAY, 1015, t);
-		sh.addTutor(Day.MONDAY, 1030, t);
-		sh.addTutor(Day.MONDAY, 1045, t);
-		sh.addTutor(Day.MONDAY, 1500, t);
-		sh.addTutor(Day.MONDAY, 1515, t);
-		sh.addTutor(Day.MONDAY, 1530, t);
-		sh.addTutor(Day.MONDAY, 1545, t);
-		sh.addTutor(Day.MONDAY, 1600, t);
-		sh.addTutor(Day.MONDAY, 1615, t);
-
-		sh.addStudent(Day.MONDAY, 1015, s);
-		sh.addStudent(Day.MONDAY, 1500, s);
 		
-		printSchedule(sh);
-		System.out.println(sh.checkAvailabilityByTutor(t, Day.MONDAY, 30));*/
-		
-		GUI gui = new GUI();
 	}
 	
 	public static void printSchedule(Scheduler s) {
