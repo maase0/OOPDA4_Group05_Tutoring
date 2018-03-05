615a9f8afcc64bd7a041f167ecf93d400d4d4317
maase0 <maase0@students.rowan.edu>
2018-03-05 17:10:19 -0500

    Completley redoing GUI

--- src/GUI.java
+++ src/GUI.java
@@ -3,70 +3,51 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-public class GUI extends JFrame{
+public class GUI{
 
-	private JButton quit;
-	private JButton switchButtonSV;
-	private JButton switchButtonTest;
-	private JPanel sv; //the schedule view
-	private JPanel test;
-	boolean view = true;
+	private JFrame frame;
+	private BorderLayout layout;
+
+	/**
+	 * Menu components
+	 */
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+	private JMenuItem openItem;
+	private JMenuItem saveItem;
+
 	
 	
 	public GUI(Pair[][] schedule) {
 		
-		setTitle("Schedule View");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		quit = new JButton("Quit");
-		switchButtonSV = new JButton("Switch");
-		switchButtonTest = new JButton("Switch");
-
-		quit.addActionListener(e -> dispose());
-
-		switchButtonSV.addActionListener(e -> {setContentPane(test); pack();});
-		switchButtonTest.addActionListener(e -> {setContentPane(sv); pack();});
+		frame = new JFrame();
+		layout = new BorderLayout();
 
 
-			
-		sv = new JPanel(new GridLayout(0, 6));
-		sv.add(switchButtonSV);
-		sv.add(quit);
-	//	addScheduleView(schedule);
-
-		test = new JPanel(new GridLayout(0, 6));
-		test.add(switchButtonTest);
+		//Initialize menu components
+		menuBar = new JMenuBar();
+		fileMenu = new JMenu("File");
+		openItem = new JMenuItem("Open");
+		saveItem = new JMenuItem("Save");
 
 
-		
-		setContentPane(sv);
-		pack();
-		
-		
-		setVisible(true);
+		//Create menu
+		frame.setLayout(layout);
+		frame.setJMenuBar(menuBar);
+		menuBar.add(fileMenu);
+		fileMenu.add(openItem);
+		fileMenu.add(saveItem);
+
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		frame.pack();
+		frame.setVisible(true);
 	}	
 
-	private void addScheduleView(Pair[][] schedule) {
-		sv.add(quit);
-		sv.add(new Label("Monday"));
-		sv.add(new Label("Tuesday"));
-		sv.add(new Label("Wednesday"));
-		sv.add(new Label("Thursday"));
-		sv.add(new Label("Friday"));
 
-		int count = 0;
-		for(int i = 1000; i < 1600; i += 100) {
-			for(int j = 0; j < 60; j += 15) {
-				sv.add(new Label((i + j) + ""));
-				for(int x = 0; x < 5; x++) {
-					sv.add(new Label(schedule[x][count].getTutor() + ": " + 
-					    schedule[x][count].getStudent()));
-				}
-				count++;
-			}
-		}
-	
-	}
-	
+
+
 	
 }
